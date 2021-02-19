fd78fbfe14a820fe3bec1bdfacaea2c0c79254f5
katamatata <51786805+katamatata@users.noreply.github.com>
2021-02-19 15:22:41 +0100

    Extract reducer into reducer.js file

--- counter-redux/src/index.js
+++ counter-redux/src/index.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import reducer from './reducer';
 
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    case 'ZERO':
-      return 0;
-    default:
-      return state;
-  }
+const store = createStore(reducer);
+
+const onButtonClicked = (action) => {
+  store.dispatch({ type: action });
 };
-
-const store = createStore(counterReducer);
 
 const Header = ({ text }) => {
   return <h1>{text}</h1>;
@@ -27,10 +19,6 @@ const Counter = ({ state }) => {
 
 const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
-};
-
-const onButtonClicked = (action) => {
-  store.dispatch({ type: action });
 };
 
 const App = () => {
