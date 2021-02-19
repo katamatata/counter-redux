0b3d340e0e53bdf34fbcf19946b54b58bbf982b5
katamatata <51786805+katamatata@users.noreply.github.com>
2021-02-19 11:50:01 +0100

    Add create react-app, cleanup boilerplate code

---
+++ counter-redux/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return <div>Counter with Redux</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
